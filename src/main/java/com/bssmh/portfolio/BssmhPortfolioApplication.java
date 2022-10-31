d26252b371547b90ed9eabc6acf9e76d62c02497
pjh0904 <pjh0904@jainwon.com>
2022-10-31 12:20:28 +0900

    git init

---
+++ src/main/java/com/bssmh/portfolio/BssmhPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.bssmh.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BssmhPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BssmhPortfolioApplication.class, args);
+	}
+
+}
