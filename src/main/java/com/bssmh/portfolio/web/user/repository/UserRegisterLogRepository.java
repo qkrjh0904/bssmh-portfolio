1039a987d5586acb3b88f260fa1da840031b6b03
pjh0904 <pjh0904@jainwon.com>
2022-11-10 16:52:24 +0900

    BSSMH-2 : Naver OAuth2 회원가입/로그인 및 로그

--- src/main/java/com/bssmh/portfolio/web/user/repository/UserRegisterLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bssmh.portfolio.web.user.repository;
-
-import com.bssmh.portfolio.db.entity.user.UserRegisterLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRegisterLogRepository extends JpaRepository<UserRegisterLog, Long> {
-}
