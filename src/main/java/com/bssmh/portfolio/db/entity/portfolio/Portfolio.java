8626584d8f7ccbc3ff06395430ad348d1a53151e
youngmin <min050410@gmail.com>
2023-03-08 20:02:03 +0900

    add: Portfolio title 칼럼 nullable 추가

--- src/main/java/com/bssmh/portfolio/db/entity/portfolio/Portfolio.java
+++ src/main/java/com/bssmh/portfolio/db/entity/portfolio/Portfolio.java
@@ -49,6 +49,7 @@ public class Portfolio extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private PortfolioTheme portfolioTheme;
 
+    @Column(nullable = false)
     private String title;
 
     @Column(columnDefinition = "text")
